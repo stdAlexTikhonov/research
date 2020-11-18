a865c32cdfcef9a6f9746134a10115160ffd435f
stdAlexTikhonov <std1992@mail.ru>
2020-11-18 18:37:56 +0300

    TASK-5 Added radio

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -8,6 +8,7 @@ import { Controls } from "../Controls";
 import { getData } from "../../utils/Data";
 import { Typography } from "@material-ui/core";
 import { Answer } from "../Answer";
+import RadioGroup from "@material-ui/core/RadioGroup";
 
 const theme = createMuiTheme({
   palette: {
@@ -26,6 +27,9 @@ export const App = () => {
   const classes = useStyles();
   const [step, setStep] = useState<number>(0);
   const [data, setData] = useState<Data[]>(() => getData());
+  const [value, setValue] = React.useState();
+
+  const handleChange = () => alert("change");
 
   return (
     <ThemeProvider theme={theme}>
@@ -36,9 +40,16 @@ export const App = () => {
             <Typography variant="h5" gutterBottom>
               {data[step].question}
             </Typography>
-            {data[step].answers.map((answer: string) => (
-              <Answer title={answer} />
-            ))}
+            <RadioGroup
+              aria-label="gender"
+              name="gender1"
+              value={value}
+              onChange={handleChange}
+            >
+              {data[step].answers.map((answer: string) => (
+                <Answer title={answer} />
+              ))}
+            </RadioGroup>
           </div>
           <Controls setStep={setStep} len={data.length} />
         </div>
