a5097f6b2a815c599c0af0912a5acd9fce2385c8
stdAlexTikhonov <std1992@mail.ru>
2020-12-09 16:44:06 +0300

    TASK-26 Titles in QuestionGroup

--- src/Components/GroupQuestion/index.tsx
+++ src/Components/GroupQuestion/index.tsx
@@ -1,9 +1,6 @@
-import React from "react";
-
-//Групповые вопросы
-//   const group_question = data.Questionary.filter(
-//     (item: any) => item.question === keys[step]
-//   );
+import React, { useEffect, useContext, useState } from "react";
+import { Context } from "../../context";
+import { Title } from "../Title";
 
 //   const question_group = group_question[0].question_group;
 
@@ -12,8 +9,30 @@ import React from "react";
 //       (item: any) => item.code === question_group
 //     )
 //   );
-// }
 
 export const GroupQuestion = () => {
-  return <div>Group Question</div>;
+  const { data, step, keys } = useContext(Context)!;
+  const [question, setQuestion] = useState<any>();
+
+  useEffect(() => {
+    //Групповые вопросы
+    const questions = data.Questionary.filter(
+      (item: any) => item.question === keys![step]
+    );
+
+    const question_group = questions[0].question_group;
+
+    setQuestion(
+      data.References.question_groups.Reference.find(
+        (item: any) => item.code === question_group
+      )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [step]);
+  return question ? (
+    <>
+      <Title title={question.value} step={step} />
+      <div>Group Question</div>
+    </>
+  ) : null;
 };
